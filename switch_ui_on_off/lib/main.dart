321127685a69e354b2bd67d14e4e11e1204e817b
smitmodi <modismit2@gmail.com>
2023-03-23 20:37:50 +0530

    Update With Hall Switch Demo

--- switch_ui_on_off/lib/main.dart
+++ switch_ui_on_off/lib/main.dart
@@ -11,105 +11,151 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Flutter Switch Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const HallSwitch());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HallSwitch extends StatefulWidget {
+  const HallSwitch({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HallSwitch> createState() => _LightSwitchState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _LightSwitchState extends State<HallSwitch> {
+  double containerHeight = 0;
+  double stickHeight = 360;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: ClipPath(
+              clipper: LightClipper(size.height),
+              child: AnimatedContainer(
+                curve: Curves.bounceOut,
+                duration: const Duration(milliseconds: 200),
+                color: bgColor,
+                height: containerHeight,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Positioned(
+            top: 355,
+            right: 50,
+            child: Container(
+              padding: const EdgeInsets.all(5),
+              height: 200,
+              width: 60,
+              decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.4),
+                  borderRadius: BorderRadius.circular(25)),
             ),
-          ],
-        ),
+          ),
+          Positioned(
+            top: 0,
+            right: 55,
+            child: Column(
+              children: [
+                AnimatedContainer(
+                  duration: const Duration(milliseconds: 200),
+                  curve: Curves.bounceOut,
+                  width: 4,
+                  height: stickHeight,
+                  color: Colors.white,
+                ),
+                GestureDetector(
+                  onVerticalDragUpdate: (details) {
+                    //print(details.delta.dy);
+                    setState(() {
+                      stickHeight += details.delta.dy;
+                    });
+                  },
+                  onVerticalDragEnd: (details) {
+                    //print('end');
+                    if (stickHeight <= 400) {
+                      setState(() {
+                        containerHeight = 0;
+                        stickHeight = 360;
+                      });
+                    } else {
+                      setState(() {
+                        containerHeight = size.height;
+                        stickHeight = 500;
+                      });
+                    }
+                  },
+                  child: const CircleAvatar(
+                    radius: 25,
+                    backgroundColor: Colors.white,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Positioned(
+            top: 0,
+            left: 29,
+            right: 0,
+            child: AppBar(
+              elevation: 0,
+              centerTitle: true,
+              backgroundColor: Colors.transparent,
+              leading: Icon(
+                Icons.arrow_back,
+                size: 35,
+                color: containerHeight == 0 ? Colors.white : Colors.black,
+              ),
+              title: Text(
+                'Learning Room',
+                style: TextStyle(
+                    fontSize: 28,
+                    color: containerHeight == 0 ? Colors.white : Colors.black,
+                    fontWeight: FontWeight.w600),
+              ),
+            ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+class LightClipper extends CustomClipper<Path> {
+  final double screenHeight;
+  LightClipper(this.screenHeight);
+
+  @override
+  Path getClip(Size size) {
+    final height = size.height;
+    final width = size.width;
+
+    double control = height >= screenHeight * 0.95 ? 0 : 40;
+
+    final path = Path();
+
+    path.lineTo(0, control);
+    path.relativeQuadraticBezierTo(width / 2, -control, width, 0);
+    path.relativeLineTo(0, height - control);
+    path.lineTo(0, height);
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
+
+Color bgColor = Color.fromARGB(255, 33, 128, 231);
